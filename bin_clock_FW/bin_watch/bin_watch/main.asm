4ca3adb8634471d1606d20535a25984dd59717da
programagor <programagor@gmail.com>
2017-08-17 18:44:13 +1000

    Added more base code
    Port initialisation after reset, and RTC timer configuration

--- bin_clock_FW/bin_watch/bin_watch/main.asm
+++ bin_clock_FW/bin_watch/bin_watch/main.asm
@@ -42,10 +42,10 @@ PC_INT0:
 PC_INT1:
 PC_INT2:
 WDT:
-TIM2_COMPA:
 TIM2_COMPB:
 TIM2_OVF:
 TIM1_CAPT:
+TIM1_COMPA:
 TIM1_COMPB:
 TIM1_OVF:
 TIM0_COMPB:
@@ -62,8 +62,8 @@ SPM_RDY:
 reti
 
 ; =============== 1 second timer ===============
-TIM1_COMPA:
-#include "pendulum.asm"
+TIM2_COMPA:
+rcall pendulum
 reti
 
 ; =============== Refresh timer ===============
@@ -73,12 +73,65 @@ TIM0_COMPA:
 ; =============== Initialisation routine here ===============
 RESET:
 ;TODO: insert initialisator
+;Need to initialise outputs immediately to stop FETs from floating
+ldi R16,0b00111111 ;port C has only 6 pins, we want all as outputs, and initially high (disabled)
+out PORTC,R16
+out DDRC,R16
 
+ldi R16,0b00000000
+out PORTD,R16
+ldi R16,0b11111111
+out DDRD,R16
+
+;Initialise stack
+ldi R16,LOW(RAMEND)
+out SPL,R16
+ldi R16,HIGH(RAMEND)
+out SPH,R16
+
+;Configure RTC
+ldi R16,1<<AS2 ;enable asynchronous operation (32.768 kHz crystal)
+sts ASSR,R16
+
+tbusy1:
+lds R16,ASSR
+sbrc R16,OCR2AUB
+rjmp tbusy1
+
+ldi R16,127 ;trigger 4 times per second
+sts OCR2A,R16
+
+ldi R16,1<<OCIE2A ;enable compare interrupt
+sts TIMSK2,R16
+
+tbusy2:
+lds R16,ASSR
+sbrc R16,TCR2AUB
+rjmp tbusy2
+ 
+ldi R16,1<<WGM21 ;CTC mode
+sts TCCR2A,R16
+ 
+tbusy3:
+lds R16,ASSR
+sbrc R16,TCR2BUB
+rjmp tbusy3
+
+ldi R16,1<<CS22 ;start clock (64 prescaler)
+sts TCCR2B,R16
+
+
+;All set up, ready to go.
+ sei
 ; =============== Sleeper loop ===============
 LOOP:
 sleep
 rjmp LOOP
 
+
+; =============== Includes ===============
+
+#include "pendulum.asm"
 
 ; =============== Global variables ===============
 .dseg
--- bin_clock_FW/bin_watch/bin_watch/pendulum.asm
+++ bin_clock_FW/bin_watch/bin_watch/pendulum.asm
@@ -4,8 +4,15 @@
 ; Created: 16/08/2017 08:32:27
 ; Author : pmg
 ;
-; This file contains the code that is to be executed every 1 second
+; This file contains the code that is to be executed every 0.25 second
 
 #include "incrementors.asm"
 
+pendulum:
+;TODO: Every fourth execution, do this:
 rcall inc1sec
+
+;TODO: If refresher isn't running, do this
+;Read buttons. If pressed, start refresher.
+
+ret
